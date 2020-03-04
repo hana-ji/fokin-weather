52c40f5ade26a341008b52e7d239a04f231403fe
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-03-04 16:08:50 +0900

    Loading Screen css 말고 네이티브 전용인 애

--- App.js
+++ App.js
@@ -1,37 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}></View>
-      <View style={styles.blueView}></View>
-    </View>
+    <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  // 부모
-  container: {
-    // 모든 공간 사용가능함을 의미함
-    flex: 1,
-    backgroundColor: '#114',
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: 'yellow',
-  },
-  blueView: {
-    flex: 3,
-    backgroundColor: 'blue',
-  },
-});
-
-/*  리액트 네이티브에서 레이아웃에 대한 규칙 
-    네이티브에서 flexbox의 디폴트는 
-    - flexDirection: column임
-    - flex: 1
-    전체 공간을 다 차지하고싶다면 flex:1
-    형제같은게 있다 (옐로 블루) 1:3 이런식으로 높이 설정 가능
-    width, height 말고 flex로 레이아웃 코딩 하자 !
-*/
---
+++ Loading.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// 스타일시트도 임폴트 하는거 잊지말자
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading(){
+  return <View style={styles.container}>
+    <Text style={styles.text}>Getting the fucking weather</Text>
+  </View>;
+  
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    // 일반적인 css에서는 찾을수 없음(리엑트 네이티브 꺼)
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA"
+  },
+  text: {
+    color: "#2c2c2c",
+    // px을 붙이면 ("20px")안에 string으로 써야함
+    fontSize: 30
+  }
+})
