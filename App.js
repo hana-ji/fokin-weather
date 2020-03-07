c6bc7f4de8ce4ca7370cca0de5196a06d6eaabb3
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-03-07 16:03:39 +0900

    Getting Location

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Loading from './Loading';
+// * = 모든것
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <Loading />
-  );
+// 이름 써줄필요 x
+export default class extends React.Component {
+  getLocation = async() => {
+    const location = await Location.getCurrentPositionAsync(options)
+  }
+  componentDidMount(){ 
+    // await functionality라서 위에 선언하고 불러옴
+    this.getLocation();
+  };
+  render(){
+    return (
+      <Loading />
+    );
+  }
 }
