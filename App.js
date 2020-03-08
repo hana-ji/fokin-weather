182c0986b0afe662e9d99d12c395c92a73bcd36b
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-03-08 12:40:19 +0900

    API 1단계

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import React, { useEffect } from 'react';
-import {Alert} from 'react-native';
-import Loading from './Loading';
-import * as Location from 'expo-location';
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "e623e85818d14b3c72a3bd75c8071736";
 
 export default class extends React.Component {
   state = {
     isLoading: true
-  }
+  };
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(
+      //  template srings임 (자바스크립트 es6버전)
+      //  중괄호 안에는 $를 넣어줘야함
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
   getLocation = async() => {
     try {
       await Location.requestPermissionsAsync();
-      // 나중에 API에 전송하고 날씨 가져올거
-      const {coords: {latitude, longitude} } = await Location.getCurrentPositionAsync();
+      const {
+        coords: {latitude, longitude} 
+      } = await Location.getCurrentPositionAsync();
+      // 같은 인자값이 함수에 넘겨짐
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false});
     } catch (error) {
-      // try 부분이 에러가 생기면 catch가 실행됨
       Alert.alert("can't find you", "So sad")
     }
   };
@@ -23,7 +36,6 @@ export default class extends React.Component {
   };
   render(){
     const { isLoading } = this.state;
-    return 
-      isLoading ? <Loading /> : null;
+    return isLoading ? <Loading /> : null;
   }
 }
